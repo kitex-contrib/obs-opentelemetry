833526b64d05b917eb4ad5dd51abca7e18116da6
Jabbi zhang <94458213+XZ0730@users.noreply.github.com>
2023-12-02 23:18:14 +0800

    feat:integrating slog to obs (#34)
    * feat:integrating slog to obs
    
    * fix:level set
    
    * fix:go.work file
    
    * fix:interface update
    
    * fix:license、tracelevel、golang version
    
    * fix:go version
    
    * fix:go version
    
    * chore: update ci goversion to 1.21
    
    * chore: use gofumpt
    
    * chore: rename options
    
    ---------
    
    Co-authored-by: kinggo <lilong.21@bytedance.com>

---
+++ logging/slog/logger.go
@@ -0,0 +1,169 @@
+// Copyright 2023 CloudWeGo Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slog
+
+import (
+	"context"
+	"io"
+	"log/slog"
+
+	"github.com/cloudwego/kitex/pkg/klog"
+)
+
+const (
+	LevelTrace  = slog.Level(-8)
+	LevelNotice = slog.Level(2)
+	LevelFatal  = slog.Level(12)
+)
+
+var _ klog.FullLogger = (*Logger)(nil)
+
+type Logger struct {
+	l      *slog.Logger
+	config *config
+}
+
+func NewLogger(opts ...Option) *Logger {
+	config := defaultConfig()
+
+	for _, opt := range opts {
+		opt.apply(config)
+	}
+
+	logger := slog.New(NewTraceHandler(config.coreConfig.writer, &config.coreConfig.opt, config.traceConfig))
+	return &Logger{
+		l:      logger,
+		config: config,
+	}
+}
+
+func (l *Logger) Log(level klog.Level, msg string, kvs ...interface{}) {
+	logger := l.l.With()
+	logger.Log(context.TODO(), tranSLevel(level), msg, kvs...)
+}
+
+func (l *Logger) Logf(level klog.Level, format string, kvs ...interface{}) {
+	logger := l.l.With()
+	msg := getMessage(format, kvs)
+	logger.Log(context.TODO(), tranSLevel(level), msg)
+}
+
+func (l *Logger) CtxLogf(level klog.Level, ctx context.Context, format string, kvs ...interface{}) {
+	logger := l.l.With()
+	msg := getMessage(format, kvs)
+	logger.Log(ctx, tranSLevel(level), msg)
+}
+
+func (l *Logger) Trace(v ...interface{}) {
+	msg, kvs := tranAtrr(v)
+	l.Log(klog.LevelTrace, msg, kvs...)
+}
+
+func (l *Logger) Debug(v ...interface{}) {
+	msg, kvs := tranAtrr(v)
+	l.Log(klog.LevelDebug, msg, kvs...)
+}
+
+func (l *Logger) Info(v ...interface{}) {
+	msg, kvs := tranAtrr(v)
+	l.Log(klog.LevelInfo, msg, kvs...)
+}
+
+func (l *Logger) Notice(v ...interface{}) {
+	msg, kvs := tranAtrr(v)
+	l.Log(klog.LevelNotice, msg, kvs...)
+}
+
+func (l *Logger) Warn(v ...interface{}) {
+	msg, kvs := tranAtrr(v)
+	l.Log(klog.LevelWarn, msg, kvs...)
+}
+
+func (l *Logger) Error(v ...interface{}) {
+	msg, kvs := tranAtrr(v)
+	l.Log(klog.LevelError, msg, kvs...)
+}
+
+func (l *Logger) Fatal(v ...interface{}) {
+	msg, kvs := tranAtrr(v)
+	l.Log(klog.LevelFatal, msg, kvs...)
+}
+
+func (l *Logger) Tracef(format string, v ...interface{}) {
+	l.Logf(klog.LevelTrace, format, v...)
+}
+
+func (l *Logger) Debugf(format string, v ...interface{}) {
+	l.Logf(klog.LevelDebug, format, v...)
+}
+
+func (l *Logger) Infof(format string, v ...interface{}) {
+	l.Logf(klog.LevelInfo, format, v...)
+}
+
+func (l *Logger) Noticef(format string, v ...interface{}) {
+	l.Logf(klog.LevelInfo, format, v...)
+}
+
+func (l *Logger) Warnf(format string, v ...interface{}) {
+	l.Logf(klog.LevelWarn, format, v...)
+}
+
+func (l *Logger) Errorf(format string, v ...interface{}) {
+	l.Logf(klog.LevelError, format, v...)
+}
+
+func (l *Logger) Fatalf(format string, v ...interface{}) {
+	l.Logf(klog.LevelFatal, format, v...)
+}
+
+func (l *Logger) CtxTracef(ctx context.Context, format string, v ...interface{}) {
+	l.CtxLogf(klog.LevelDebug, ctx, format, v...)
+}
+
+func (l *Logger) CtxDebugf(ctx context.Context, format string, v ...interface{}) {
+	l.CtxLogf(klog.LevelDebug, ctx, format, v...)
+}
+
+func (l *Logger) CtxInfof(ctx context.Context, format string, v ...interface{}) {
+	l.CtxLogf(klog.LevelInfo, ctx, format, v...)
+}
+
+func (l *Logger) CtxNoticef(ctx context.Context, format string, v ...interface{}) {
+	l.CtxLogf(klog.LevelWarn, ctx, format, v...)
+}
+
+func (l *Logger) CtxWarnf(ctx context.Context, format string, v ...interface{}) {
+	l.CtxLogf(klog.LevelWarn, ctx, format, v...)
+}
+
+func (l *Logger) CtxErrorf(ctx context.Context, format string, v ...interface{}) {
+	l.CtxLogf(klog.LevelError, ctx, format, v...)
+}
+
+func (l *Logger) CtxFatalf(ctx context.Context, format string, v ...interface{}) {
+	l.CtxLogf(klog.LevelFatal, ctx, format, v...)
+}
+
+func (l *Logger) SetLevel(level klog.Level) {
+	lvl := tranSLevel(level)
+	l.config.coreConfig.level.Set(lvl)
+}
+
+func (l *Logger) SetOutput(writer io.Writer) {
+	log := slog.New(NewTraceHandler(writer, &l.config.coreConfig.opt, l.config.traceConfig))
+	l.config.coreConfig.writer = writer
+	l.l = log
+}
---
+++ logging/slog/logger_test.go
@@ -0,0 +1,121 @@
+// Copyright 2023 CloudWeGo Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package slog
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"strings"
+	"testing"
+
+	"github.com/cloudwego/kitex/pkg/klog"
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+)
+
+func stdoutProvider(ctx context.Context) func() {
+	provider := sdktrace.NewTracerProvider()
+	otel.SetTracerProvider(provider)
+
+	exp, err := stdouttrace.New(stdouttrace.WithPrettyPrint())
+	if err != nil {
+		panic(err)
+	}
+
+	bsp := sdktrace.NewBatchSpanProcessor(exp)
+	provider.RegisterSpanProcessor(bsp)
+
+	return func() {
+		if err := provider.Shutdown(ctx); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func TestLogger(t *testing.T) {
+	ctx := context.Background()
+
+	buf := new(bytes.Buffer)
+
+	shutdown := stdoutProvider(ctx)
+	defer shutdown()
+
+	logger := NewLogger(
+		WithTraceErrorSpanLevel(slog.LevelWarn),
+		WithRecordStackTraceInSpan(true),
+	)
+
+	klog.SetLogger(logger)
+	klog.SetOutput(buf)
+	klog.SetLevel(klog.LevelDebug)
+
+	logger.Info("log from origin zap")
+	assert.True(t, strings.Contains(buf.String(), "log from origin zap"))
+	buf.Reset()
+
+	tracer := otel.Tracer("test otel std logger")
+
+	ctx, span := tracer.Start(ctx, "root")
+
+	klog.CtxInfof(ctx, "hello %s", "world")
+	assert.True(t, strings.Contains(buf.String(), "trace_id"))
+	assert.True(t, strings.Contains(buf.String(), "span_id"))
+	assert.True(t, strings.Contains(buf.String(), "trace_flags"))
+	buf.Reset()
+
+	span.End()
+
+	ctx, child := tracer.Start(ctx, "child")
+
+	klog.CtxWarnf(ctx, "foo %s", "bar")
+
+	klog.CtxTracef(ctx, "trace %s", "this is a trace log")
+	klog.CtxDebugf(ctx, "debug %s", "this is a debug log")
+	klog.CtxInfof(ctx, "info %s", "this is a info log")
+	klog.CtxNoticef(ctx, "notice %s", "this is a notice log")
+	klog.CtxWarnf(ctx, "warn %s", "this is a warn log")
+	klog.CtxErrorf(ctx, "error %s", "this is a error log")
+
+	child.End()
+
+	_, errSpan := tracer.Start(ctx, "error")
+
+	klog.Info("no trace context")
+
+	errSpan.End()
+}
+
+func TestLogLevel(t *testing.T) {
+	buf := new(bytes.Buffer)
+
+	logger := NewLogger(
+		WithTraceErrorSpanLevel(slog.LevelWarn),
+		WithRecordStackTraceInSpan(true),
+	)
+
+	// output to buffer
+	logger.SetOutput(buf)
+
+	logger.Debug("this is a debug log")
+	assert.False(t, strings.Contains(buf.String(), "this is a debug log"))
+
+	logger.SetLevel(klog.LevelDebug)
+
+	logger.Debugf("this is a debug log %s", "msg")
+	assert.True(t, strings.Contains(buf.String(), "this is a debug log"))
+}
